2df134bc5c2df4137612ab9115204505ac1e8e2b
Sefer <Sefer@DESKTOP-5O93JGC>
2021-01-22 18:45:28 +0300

    Add project files.

---
+++ Demo1/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo1
+{
+    class MusteriManager
+    {
+        public void AddNewCustomer(Musteri musteri)
+        {
+            System.Console.WriteLine("--------------------");
+            System.Console.WriteLine("Hoşgeldin " + musteri.FirstName + " " + musteri.LastName + "!");
+            System.Console.WriteLine("--------------------");
+        }
+        public void DeleteCustomer(Musteri musteri)
+        {
+            System.Console.WriteLine("--------------------");
+            System.Console.WriteLine("Hoşçakal " + musteri.FirstName + " " + musteri.LastName + "  ");
+            System.Console.WriteLine("--------------------");
+        }
+        public void list(Musteri musteri)
+        {
+            Console.WriteLine(musteri.FirstName + " " + musteri.LastName + " " + "Listelendi.");
+        }
+    }
+}
---
+++ Demo1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 100;
+            musteri1.FirstName = "ahmet";
+            musteri1.LastName = "çakar";
+            musteri1.MusteriNo = 1;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.FirstName = "Zengin";
+            musteri2.LastName = "Paralı";
+            musteri2.Id = 101;
+            musteri2.MusteriNo = 2;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.FirstName = "Yusuf";
+            musteri3.LastName = "Günay";
+            musteri3.Id = 102;
+            musteri3.MusteriNo = 3;
+
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.AddNewCustomer(musteri1);
+            musteriManager.DeleteCustomer(musteri2);
+            musteriManager.list(musteri3);
+
+        }
+    }
+}
